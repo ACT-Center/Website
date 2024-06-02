ac5df5f2e430377084e3f1f8d5f12cfb7e109458
mesaaransh <mesaaransh@gmail.com>
2024-06-03 00:10:15 +0530

    navbar and responsive changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./NavBar.css"
 import { Outlet, Link } from 'react-router-dom';
 import logo from "../../assets/logo.webp"
 
 export default function NavBar() {
 
+    const [isMobile, setIsMobile] = useState('none');
+    function mobileNavHandle(){
+        if (isMobile == 'block') {
+            setIsMobile('none')
+        }
+        else{
+            setIsMobile('block')
+        }
+    }
+
     return (
-        <div className='navbar'>
+        <>
+        <div className="mobileMenu" style={{display: isMobile}}>
+            <div className="mobileMenuTopBar">
+                <span class="material-symbols-outlined" onClick={() => {mobileNavHandle()}}>cancel</span>
+            </div>
+            <div className="mobileMenuMenu">
+                <a className='ourNavMenuItem' href='/'>Home</a>
+                <a className='ourNavMenuItem' href='/about'>About</a>
+                <a className='ourNavMenuItem' href='resources'>Resources</a>
+                <a className='ourNavMenuItem' href='/submission'>Submission</a>
+                {/* <a className='ourNavMenuItem' href='/support'>Support</a> */}
+                <a className='ourNavMenuItem' href='/evaluation'>Evaluation</a>
+                <a className='ourNavMenuItem' href='/contact'>Contact</a>
+            </div>
+        </div>
+        <div className='ourNavbar navbar-expand-lg'>
             <div className="logo">
                 <img src={logo} alt="" />
                 ACT CENTRE
             </div>
-            <div className="navMenu">
-                <a className='navMenuItem' href='/'>Home</a>
-                <a className='navMenuItem' href='/about'>About</a>
-                <a className='navMenuItem' href='resources'>Resources</a>
-                <a className='navMenuItem' href='/submission'>Submission</a>
-                {/* <a className='navMenuItem' href='/support'>Support</a> */}
-                <a className='navMenuItem' href='/evaluation'>Evaluation</a>
-                <a className='navMenuItem' href='/contact'>Contact</a>
+            <div className="hamburgerCont" onClick={() => {mobileNavHandle()}}>
+                <span class="material-symbols-outlined">menu</span>
+            </div>
+            <div className="ourNavMenu">
+                <a className='ourNavMenuItem' href='/'>Home</a>
+                <a className='ourNavMenuItem' href='/about'>About</a>
+                <a className='ourNavMenuItem' href='resources'>Resources</a>
+                <a className='ourNavMenuItem' href='/submission'>Submission</a>
+                {/* <a className='ourNavMenuItem' href='/support'>Support</a> */}
+                <a className='ourNavMenuItem' href='/evaluation'>Evaluation</a>
+                <a className='ourNavMenuItem' href='/contact'>Contact</a>
             </div>
         </div>
+        </>
     )
 }
